377280d5070838cc7b3b81a8fbb11fc36ba7dff0
Claire Hardgrove <clairehardgrove@gmail.com>
2019-08-09 09:34:01 +1000

    add circular_buffer to install script

--- limiter/setup.py
+++ limiter/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name='limiter',    # This is the name of your PyPI-package.
     version='0.1',                          # Update the version number for new releases
-    scripts=['limiter.py']                     # The name of your scipt, and also the command you'll be using for calling it
+    scripts=['limiter.py','circular_buffer.py']  # The name of your scipt, and also the command you'll be using for calling it
 )
